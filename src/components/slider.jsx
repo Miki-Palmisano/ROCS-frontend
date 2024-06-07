617662981e72036488a20ecfd4df7597960b940d
Miki-Palmisano <palmmiki21@gmail.com>
2024-06-07 15:42:57 +0200

    insert API

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -3,39 +3,21 @@ import '../styles/slider.css';
 import rullino from '../resources/rullino.jpg';
 import Card from './card';
 
-const image = [
-    'https://images-3.rakuten.tv/storage/global-movie/translation/artwork/3bb2d783-d9fb-4022-83da-449147ef90c9.jpeg',
-    'https://lumiere-a.akamaihd.net/v1/images/image_9758fdef.jpeg?region=0,0,540,810',
-    'https://pad.mymovies.it/filmclub/2023/04/019/locandinapg1.jpg',
-    'https://pad.mymovies.it/filmclub/2017/09/127/locandina.jpg',
-    'https://www.raiplay.it/cropgd/300x400/dl/img/2024/04/30/1714462669364_1536x2048_logo.jpg',
-    'https://www.stateofmind.it/wp-content/uploads/2021/02/Coco-2017-un-esempio-di-Death-Education-Recensione-del-film-Disney-Featured.jpg.webp',
-    'https://www.indigofilm.it/wp-content/uploads/2023/10/Comandante_Main-KA-140x200-1-560x800.jpg?v=1696865305',
-    'https://www.italyformovies.it/app/img/editor/mceu_91117987711700216977534.jpg?1700216977876',
-    'https://www.warnerbros.it/wp-content/uploads/2022/11/Tre-di-Troppo_Poster-Italia.jpg',
-    'https://mr.comingsoon.it/imgdb/locandine/big/63166.jpg',
-    'https://movieplayer.net-cdn.it/t/images/2023/12/06/succede-anche-nelle-migliori-famiglie-poster_jpg_320x0_crop_q85.jpg',
-    'https://foto1.newsauto.it/wp-content/uploads/2023/09/Locandina-film-Granturismo.jpg',
-    'https://pad.mymovies.it/filmclub/2022/02/239/locandinapg1.jpg',
-    'https://static.posters.cz/image/350/poster/rocky-balboa-film-rocky-i167759.jpg'
-  ];
+export default function App({films, loading}) {
 
-export default function App() {
-  const [images, setImages] = useState(image);
-
-  return (
+  return ( 
     <div className="slider">
         <div className="rullino-container"> 
-            {images.map(() => <img src={rullino} alt="rullino fotografico" /> )}
+            {films.map(() => <img src={rullino} alt="rullino fotografico" /> )}
         </div>
         <nav className="images-container">
-            {images.map((image, index) => ( <Card image={image} index={index}/> ))}
+            {loading ? <h1>Loading...</h1> : films.map((f, index) => ( <Card image={f.img} index={index}/> ))}
         </nav>
         <div className="rullino-container"> 
-            {images.map(() => <img src={rullino} alt="rullino fotografico" /> )}
+            {films.map(() => <img src={rullino} alt="rullino fotografico" /> )}
         </div>
-    </div>
-  );
+    </div> 
+  ); 
 };
 
 /*
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,30 @@
+import { useEffect, useState } from "react";
 import Header from "../components/header";
 import Slider from "../components/slider";
+import { set } from "mongoose";
 
 
 export default function Home() {
+  const [films, setFilms] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/api/film').then((res)=>{
+      res.json().then((data)=>{
+        setFilms(data);
+        setLoading(false);
+      })
+    })
+  }, []);
+
+
   return (
     <>
-    <Header />
-    <h1 style={{color:'#fff'}}>I più popolari</h1>
-    <Slider />
+    <Header/>
+    <h1 style={{color:'#fff'}}>Film</h1>
+    <Slider films={films} loading={loading}/>
     <h1 style={{color:'#fff'}}>Solo perché sei tu</h1>
-    <Slider />
+    <Slider films={films} loading={loading}/>
     </>
   );
 }
