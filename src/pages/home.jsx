484f153b9822ee70f9be46ff0840da35bce0a780
Miki-Palmisano <palmmiki21@gmail.com>
2024-06-19 14:41:18 +0200

    MicroService Update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,29 +3,26 @@ import Slider from "../components/slider";
 import Header from "../components/header";
 import axios from "axios";
 
-
 export default function Home() {
-  const url = 'https://rocs-api.onrender.com';
+  const GATEWAY_API = process.env.REACT_APP_API_GATEWAY_URL;
   const [films, setFilms] = useState([]);
   const [loadingFilm, setLoadingFilm] = useState(true);
   const [series, setSeries] = useState([]);
   const [loadingSeries, setLoadingSeries] = useState(true);
 
-  
   useEffect(() => {
-    axios.get(`${url}/api/film`).then((res)=>{
+    axios.get(`${GATEWAY_API}/content/films`).then((res)=>{
       setFilms(res.data);
       setLoadingFilm(false);
     }).catch( e => console.log(e));
-  }, []);
+  }, [GATEWAY_API]);
 
   useEffect(() => { 
-    axios.get(`${url}/api/serie`).then((res)=>{
+    axios.get(`${GATEWAY_API}/content/series`).then((res)=>{
       setSeries(res.data);
       setLoadingSeries(false);
     }).catch( e => console.log(e));
-  }, []);
-
+  }, [GATEWAY_API]);
 
   return (
     <>
